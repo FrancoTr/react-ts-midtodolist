aeef37561e1d9e2c34e8cff1490124100f5e191a
Franco Traverso <franco-t20@outlook.com>
2022-09-02 23:33:35 -0300

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>List view</div>} />
+        <Route path='/focus' element={<div>Focus view</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
