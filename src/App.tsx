22b2e896bcd54b96b0fa9494ac3632b0393264e3
Franco Traverso <franco-t20@outlook.com>
2022-09-03 22:57:10 -0300

    Add list view component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ListScreen from "./screens/ListScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>List view</div>} />
+        <Route path='/' element={<ListScreen />} />
         <Route path='/focus' element={<div>Focus view</div>} />
       </Routes>
     </BrowserRouter>
