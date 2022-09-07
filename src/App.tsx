1c87c8d9c543a2b7f8354ae779b3639ecae1b178
Franco Traverso <franco-t20@outlook.com>
2022-09-06 22:01:21 -0300

    Add basic navigation functionality between list & focus view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import ListScreen from "./screens/ListScreen";
 
 function App() {
+  const activeStyle = {
+    fontWeight: "bold",
+  };
+
   return (
     <BrowserRouter>
+      <nav>
+        <NavLink to='/' style={({ isActive }) => (isActive ? activeStyle : {})}>
+          List View
+        </NavLink>{" "}
+        -{" "}
+        <NavLink to='/focus' style={({ isActive }) => (isActive ? activeStyle : {})}>
+          Focus View
+        </NavLink>
+      </nav>
+      <br />
       <Routes>
         <Route path='/' element={<ListScreen />} />
         <Route path='/focus' element={<div>Focus view</div>} />
