591b8112aeff63b668a4d9bb20f171fcd88eb110
Franco Traverso <franco-t20@outlook.com>
2022-09-07 21:55:11 -0300

    Test tasks array going from list screen to focus screen

--- src/screens/FocusScreen.tsx
+++ src/screens/FocusScreen.tsx
@@ -3,8 +3,9 @@ import { TasksProps } from "../types";
 
 type Props = TasksProps & {};
 
-const FocusScreen: React.FC<Props> = () => {
-  return <div>Focus Screen</div>;
+const FocusScreen: React.FC<Props> = ({ tasks }) => {
+  const task = tasks[0];
+  return task ? <div>{task.label}</div> : <div>No tasks</div>;
 };
 
 export default FocusScreen;
