f13a36dcb1eeee3b481c37908ee659f0321eb480
Franco Traverso <franco-t20@outlook.com>
2022-09-07 21:31:22 -0300

    Add focus screen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import FocusScreen from "./screens/FocusScreen";
 import ListScreen from "./screens/ListScreen";
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
       <br />
       <Routes>
         <Route path='/' element={<ListScreen />} />
-        <Route path='/focus' element={<div>Focus view</div>} />
+        <Route path='/focus' element={<FocusScreen />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/screens/FocusScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const FocusScreen: React.FC<Props> = () => {
+  return <div>Focus Screen</div>;
+};
+
+export default FocusScreen;
