986ca6cdb209c31d5a53b8284e92df16f443c459
Franco Traverso <franco-t20@outlook.com>
2022-09-14 20:55:56 -0300

    Add function that shuffles incomplete tasks to be randomly displayed in focused screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import FocusScreen from "./screens/FocusScreen";
 import ListScreen from "./screens/ListScreen";
 import { Task } from "./types";
+import { shuffle } from "lodash";
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(undefined);
 
   const updateTaskCompletion = (taskId: string, isComplete: boolean) => {
     setTasks((tasks) =>
@@ -16,7 +18,11 @@ function App() {
     );
   };
 
-  const focusedTask = tasks.filter((task) => !task.isComplete)[0];
+  const focusedTask = tasks.find((task) => task.id === focusedTaskId);
+
+  const shuffleFocusedTask = () => {
+    setFocusedTaskId(shuffle(tasks.filter((task) => !task.isComplete))[0]?.id);
+  };
 
   const tasksApi = { focusedTask, tasks, setTasks, updateTaskCompletion };
   const activeStyle = {
