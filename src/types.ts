2e7ffc840bc9de2161c982bb81edaf51db9af771
Franco Traverso <franco-t20@outlook.com>
2022-09-14 21:19:52 -0300

    Add shuffling to focused screen, nope button focus on another incomplete task

--- src/types.ts
+++ src/types.ts
@@ -9,5 +9,6 @@ export type TasksProps = {
   focusedTask?: Task;
   tasks: Task[];
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+  shuffleFocusedTask: () => void;
   updateTaskCompletion: (taskId: string, isComplete: boolean) => void;
 };
