90d71ed1c175b1d366688fd72db51f7c9aa036f2
Franco Traverso <franco-t20@outlook.com>
2022-09-19 23:12:12 -0300

    Add internal state that handles the incoming one

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 const useLocalStorage = <TState>(key: string, newState: TState) => {
-  const stateStr = window.localStorage.getItem(key);
-  const state = stateStr ? JSON.parse(stateStr) : newState;
+  const [state, setState] = useState<TState>(() => {
+    const stateStr = window.localStorage.getItem(key);
+    return stateStr ? (JSON.parse(stateStr) as TState) : newState;
+  });
+
+  const updateState = (state: TState) => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  };
+
+  return [state, updateState];
 };
 
 export default useLocalStorage;
