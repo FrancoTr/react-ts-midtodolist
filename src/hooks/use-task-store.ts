ba43147d9eb389d4d7aa4a4d1669f0fdf09d315f
Franco Traverso <franco-t20@outlook.com>
2022-09-25 21:31:48 -0300

    Fix completed tasks not being updated in the focus screen

--- src/hooks/use-task-store.ts
+++ src/hooks/use-task-store.ts
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { shuffle } from "lodash";
 import { nanoid } from "nanoid";
 import { useState } from "react";
@@ -7,8 +7,9 @@ import { Task } from "../types";
 
 const useTaskStore = () => {
   const [tasks, setTasks] = useContext(TaskContext);
-  const getIncompleteTask = () => tasks.filter((task) => !task.isComplete)[0]?.id;
-  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(getIncompleteTask());
+  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(
+    tasks.filter((task) => !task.isComplete)[0]?.id
+  );
 
   const addTask = (task: Pick<Task, "label">) => {
     const id = nanoid();
@@ -28,6 +29,12 @@ const useTaskStore = () => {
   };
 
   const focusedTask = tasks.find((task) => task.id === focusedTaskId);
+
+  useEffect(() => {
+    if (focusedTask?.isComplete) {
+      setFocusedTaskId(tasks.filter((task) => !task.isComplete)[0]?.id);
+    }
+  }, [tasks, focusedTask]);
 
   const shuffleFocusedTask = () => {
     setFocusedTaskId(shuffle(tasks.filter((task) => !task.isComplete))[0]?.id);
