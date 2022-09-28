193aadc3f8a138c6624ce9c2e4d46c0b8fa77159
Franco Traverso <franco-t20@outlook.com>
2022-09-27 22:59:16 -0300

    Add tab button styled component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import ListScreen from "./screens/ListScreen";
 import { Task } from "./types";
 import useLocalStorage from "./hooks/use-local-storage";
 import TaskContext from "./contexts/task-store";
+import styled from "styled-components";
+
+const TabButton = styled(NavLink)`
+  width: 120px;
+  color: #fff;
+  height: 62px;
+  width: 120px;
+`;
 
 function App() {
   const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", []);
@@ -13,8 +21,8 @@ function App() {
     <BrowserRouter>
       <TaskContext.Provider value={[tasks, setTasks]}>
         <nav>
-          <NavLink to='/'>List View</NavLink>
-          <NavLink to='/focus'>Focus View</NavLink>
+          <TabButton to='/'>List View</TabButton>
+          <TabButton to='/focus'>Focus View</TabButton>
         </nav>
         <br />
         <Routes>
