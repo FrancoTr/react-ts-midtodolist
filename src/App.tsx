15aa4c18f024974fc0db9661751cd48afdab30c0
Franco Traverso <franco-t20@outlook.com>
2022-09-27 22:42:38 -0300

    Remove original navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,18 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import FocusScreen from "./screens/FocusScreen";
 import ListScreen from "./screens/ListScreen";
 import { Task } from "./types";
-import { shuffle } from "lodash";
-import { nanoid } from "nanoid";
 import useLocalStorage from "./hooks/use-local-storage";
 import TaskContext from "./contexts/task-store";
 
 function App() {
   const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", []);
 
-  const activeStyle = {
-    fontWeight: "bold",
-  };
-
   return (
     <BrowserRouter>
       <TaskContext.Provider value={[tasks, setTasks]}>
         <nav>
-          <NavLink to='/' style={({ isActive }) => (isActive ? activeStyle : {})}>
-            List View
-          </NavLink>{" "}
-          -{" "}
-          <NavLink to='/focus' style={({ isActive }) => (isActive ? activeStyle : {})}>
-            Focus View
-          </NavLink>
+          <NavLink to='/'>List View</NavLink>
+          <NavLink to='/focus'>Focus View</NavLink>
         </nav>
         <br />
         <Routes>
