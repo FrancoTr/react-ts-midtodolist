6fc6daa23bceae57214ba598377d66ae94e0880e
Franco Traverso <franco-t20@outlook.com>
2022-10-12 23:20:08 -0300

    Fix syntax typo

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -27,7 +27,7 @@ const StyledCheckbox = styled.div<{ checked: boolean }>`
   width: 25px;
   height: 25px;
   background: ${(props) => (props.checked ? colors.primary : "none")};
-  border: 3px solid ${colors.primary}
+  border: 3px solid ${colors.primary};
   border-radius: 25px;
   transition: all 100ms;
 `;
